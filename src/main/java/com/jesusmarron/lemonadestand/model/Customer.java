50dc64393c637791e7297f6a8806bb8e93957693
Jesus Marron <jesusmarron7@gmail.com>
2023-06-07 14:27:32 -0500

    Customer getters and setters

--- src/main/java/com/jesusmarron/lemonadestand/model/Customer.java
+++ src/main/java/com/jesusmarron/lemonadestand/model/Customer.java
@@ -1,4 +1,23 @@
 package com.jesusmarron.lemonadestand.model;
 
 public class Customer {
+
+    private String name;
+    private String phoneNumber;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
