cad7224e2e274ae6bbe1f1df4a6372dd66b8d586
Jesus Marron <jesusmarron7@gmail.com>
2023-06-07 14:50:59 -0500

    Order class

--- src/main/java/com/jesusmarron/lemonadestand/model/Order.java
+++ src/main/java/com/jesusmarron/lemonadestand/model/Order.java
@@ -1,5 +1,41 @@
 package com.jesusmarron.lemonadestand.model;
 
-public class Order {
+import java.util.Arrays;
 
+public class Order {
+    private Customer customer;
+    private Lemonade[] lemonades;
+    private double total;
+
+    public Order(Customer customer) {
+        this.customer = customer;
+        lemonades = new Lemonade[0];
+    }
+
+    private void updateTotal() {
+        for (Lemonade l : lemonades) {
+            total += l.getPrice();
+        }
+    }
+
+    public void addLemonade(Lemonade lemonade) {
+       Lemonade[] temp = Arrays.copyOf(lemonades, lemonades.length+1);
+
+       temp[temp.length-1] = lemonade;
+       lemonades = temp;
+
+       updateTotal();
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Lemonade[] getLemonades() {
+        return lemonades;
+    }
+
+    public double getTotal() {
+        return total;
+    }
 }
